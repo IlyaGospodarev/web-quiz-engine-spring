a150db19fa887ed77013a3a72048fd020ad2daef
Ilya Gospodarev <gospodarev88@gmail.com>
2023-03-29 10:40:56 +0200

    Our Bean Component. Used in response to our POST request when the user wants to answer a quiz.

---
+++ Web Quiz Engine (Java)/task/src/engine/model/response/AnswerResponse.java
@@ -0,0 +1,33 @@
+package engine.model.response;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnswerResponse {
+    private boolean success;
+
+    private String feedback;
+
+    public AnswerResponse() {}
+
+    public AnswerResponse(boolean success, String feedback) {
+        this.success = success;
+        this.feedback = feedback;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}

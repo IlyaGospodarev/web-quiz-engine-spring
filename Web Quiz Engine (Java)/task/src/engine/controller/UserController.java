c9af40c48f843499ac63e3519d3fb542c5913df3
Ilya Gospodarev <gospodarev88@gmail.com>
2023-03-31 12:47:13 +0200

    Creating user controller.

---
+++ Web Quiz Engine (Java)/task/src/engine/controller/UserController.java
@@ -0,0 +1,26 @@
+package engine.controller;
+
+import engine.entity.User;
+import engine.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public void registerUser(@RequestBody @Valid User user) {
+        userService.registerNewUser(user.getUsername(), user.getPassword());
+    }
+}

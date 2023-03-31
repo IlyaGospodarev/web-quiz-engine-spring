5ada8a6934ae57e6e586fafa1db18a12858825bc
Ilya Gospodarev <gospodarev88@gmail.com>
2023-03-31 18:48:53 +0200

    Refactoring security classes.

--- Web Quiz Engine (Java)/task/src/engine/security/WebSecurityConfig.java
+++ Web Quiz Engine (Java)/task/src/engine/security/WebSecurityConfig.java
@@ -2,16 +2,12 @@ package engine.security;
 
 import engine.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,13 +16,17 @@ import javax.servlet.http.HttpServletResponse;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final BCryptPasswordEncoder encoder;
+
     private final UserService userService;
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
 
     @Autowired
     public WebSecurityConfig(BCryptPasswordEncoder encoder,
-                             UserService userDetailsService) {
+                             UserService userDetailsService, AuthenticationEntryPoint authenticationEntryPoint) {
         this.encoder = encoder;
         this.userService = userDetailsService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
     }
 
     @Override
@@ -36,19 +36,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+        http
+                .httpBasic().authenticationEntryPoint(authenticationEntryPoint)
                 .and()
-                .csrf().disable().headers().frameOptions().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint((req, resp, e) ->
                                                   resp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
                 .and()
                 .authorizeRequests()
-                .antMatchers("/quizzes", "/quizzes/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers("/api/register", "/actuator/shutdown").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .csrf().disable().headers().frameOptions().disable();
     }
 }
